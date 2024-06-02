c43c054c0b99021dfa3bc41e656f2de4a852a199
Smolderbrand <59597833+Smolderbrand@users.noreply.github.com>
2024-06-02 08:50:38 +0300

    Changes after loom

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
 var words = [];
 
 function clickSave() {
-	var word = document.getElementById("NewWord").value;
+	var word = document.getElementById("new_word").value;
 	var wordsdisplay = "Words: ";
 	words.push(word);
-	for (var i = 0; i < words.length; ++i)
+	for (var i = 0; i < words.length; ++i) {
 		wordsdisplay += words[i] + " ";
+	}
 	document.getElementById("Words").textContent = wordsdisplay;
 }
 
 function clickSearch() {
-	var fl = 0;
-	var word = document.getElementById("SearchWord").value;
-	for (var i = 0; i < words.length; ++i)
-		if (words[i].toLowerCase() == word.toLowerCase())
-			fl = 1;
-	if (fl == 1) {
+	var flag = 0;
+	var word = document.getElementById("search_word").value;
+	for (var i = 0; i < words.length; ++i) {
+		if (words[i].toLowerCase() == word.toLowerCase()) {
+			flag = 1;
+		}
+	}
+	if (flag == 1) {
 		document.getElementById("Answer").textContent = "Found in dictionary";
 	} else {
 		document.getElementById("Answer").textContent = "Not found in dictionary";
